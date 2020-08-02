6e1e09579e459709d6b6dbfdaeb65ba16ae96e4f
Jojo Zhang <jojozhang0327@gmail.com>
2020-08-02 16:12:40 -0400

    update type string

--- main.js
+++ main.js
@@ -12,11 +12,11 @@ instance
   .deleteAll()
   .typeString("I'm a full stack<br>web developer.")
   .pauseFor(500)
-  .deleteChars(26)
-  .typeString(
-    "passionate about<br>learning new technologies<br>and making ideas come true.",
-  )
-  .pauseFor(500)
+  // .deleteChars(26)
+  // .typeString(
+  //   "passionate about<br>learning new technologies<br>and making ideas come true.",
+  // )
+  // .pauseFor(500)
   .deleteAll()
   .typeString("Check out my work at<br>github.com/nomadkitty")
   .pauseFor(500)
